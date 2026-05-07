f251def427c079ced416aa90e7115960e0020686
otavro <otavio.reis366@gmail.com>
2026-05-06 23:53:32 -0300

    feat(beecrowd): add problem 1003 - Simple Sum  solution

---
+++ problems/beecrowd/beginner/1003.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+int main() {
+
+    int numberA, numberB;
+
+    std::cin >> numberA >> numberB;
+    std::cout << "SOMA = " << numberA + numberB << std::endl;
+    
+
+    return 0;
+}
