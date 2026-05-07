429f0051d6ab1c814b1e15bf72a4c5e49452c0a1
otavro <otavio.reis366@gmail.com>
2026-05-06 23:41:12 -0300

    feat(beecrowd): add problem 1001 - Extremely Basic  solution

---
+++ problems/beecrowd/beginner/1001.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+int main() {
+
+    int numberA, numberB;
+
+    std::cin >> numberA >> numberB;
+    std::cout << "X = " << numberA + numberB << std::endl;
+
+    return 0;
+}
