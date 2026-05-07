341df96bbef70bf45a1bf3529837a656dc2ab930
otavro <otavio.reis366@gmail.com>
2026-05-06 23:54:54 -0300

    feat(beecrowd): add problem 1004 - Simple Product  solution

---
+++ problems/beecrowd/beginner/1004.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+int main() {
+
+    int numberA, numberB;
+
+    std::cin >> numberA >> numberB;
+    std::cout << "PROD = " << numberA * numberB << std::endl;
+    
+
+    return 0;
+}
