3cad9a88bd238da52892640f5da5aa0e9e6c77b4
otavro <otavio.reis366@gmail.com>
2026-05-17 19:56:06 -0300

    feat(beecrowd): add problem 2747 - Output 1 - solution

---
+++ problems/beecrowd/beginner/2747.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+   
+    string ans = "---------------------------------------\n|                                     |\n|                                     |\n|                                     |\n|                                     |\n|                                     |\n---------------------------------------\n";
+
+    cout << ans;
+
+
+    return 0;
+}
