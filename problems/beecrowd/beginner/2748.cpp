17458d510ad0a01dbe549652d66fe1122f3d553a
otavro <otavio.reis366@gmail.com>
2026-05-17 20:00:49 -0300

    feat(beecrowd): add problem 2748 - Output 2 - solution

---
+++ problems/beecrowd/beginner/2748.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+   
+
+    cout << "---------------------------------------\n";
+    cout << "|        Roberto                      |\n";
+    cout << "|                                     |\n";
+    cout << "|        5786                         |\n";
+    cout << "|                                     |\n";
+    cout << "|        UNIFEI                       |\n";
+    cout << "---------------------------------------\n";
+
+
+    return 0;
+}
