56fdd547c1a589eddb8c13d3ad084f18fe91f5cd
otavro <otavio.reis366@gmail.com>
2026-05-17 21:28:29 -0300

    feat(beecrowd): add problem 1040 - Average 3 - solution

--- problems/beecrowd/beginner/1040.cpp
+++ problems/beecrowd/beginner/1040.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+void isStudentApproved(double mean){
+    if(mean >= 6){
+        cout << "Aluno aprovado." << endl;
+    }
+    else {
+        cout << "Aluno reprovado." << endl;
+    }
+}
+
 int main() {
 
-    int i, j=7, count;
+    double mean, n1, n2, n3, n4, exam;
 
-    for(i = 1; i <=9; i+=2){
-        for(count=0; count<3; count++){
-            cout << "I=" << i << " J=" << j << endl;
-            j--;
-        }
-        j += 5;
+    cin >> n1 >> n2 >> n3 >> n4;
+
+    mean = ((n1 * 2) + (n2 * 3) + (n3 * 4) + (n4 * 1)) / 10;
+    
+    cout << fixed << setprecision(1);
+    cout << "Media: " << mean << endl;
+    
+    if(mean >=5 && mean <= 6.9){
+        cout << "Aluno em exame." << endl;
+        cin >> exam;
+        cout << "Nota do exame: " << exam << endl;
+        
+        mean = (mean + exam) / 2;
+        isStudentApproved(mean);
+        cout << "Media final: " << mean << endl;
+
+    }else{
+        isStudentApproved(mean);
     }
+    
+    
 
     return 0;
 }
