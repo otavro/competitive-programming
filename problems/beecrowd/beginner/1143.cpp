dba3915375840b14f71e6571b912407cab133de2
otavro <otavio.reis366@gmail.com>
2026-05-19 23:12:07 -0300

    feat(beecrowd): add problem 1143 - Squared and Cubic - solution

---
+++ problems/beecrowd/beginner/1143.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        cout << i << " " << i * i << " " << i *i* i << "\n";
+    }
+
+    return 0;
+}
