9c3903b9dd06b9781e7cecc09b34523fd924e96d
otavro <otavio.reis366@gmail.com>
2026-05-19 23:27:44 -0300

    feat(beecrowd): add problem 1078 - Multiplication Table - solution

---
+++ problems/beecrowd/beginner/1078.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    
+    cin >> n;
+
+    for(int i = 1; i<=10; i++){
+        cout << i << " x " << n << " = " << i * n << "\n";
+    }
+    
+    
+    return 0;
+}
