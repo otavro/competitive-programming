80c1596c19b640caf6190cc4542d8b5ed706c8b1
otavro <otavio.reis366@gmail.com>
2026-05-19 23:25:02 -0300

    feat(beecrowd): add problem 1116 - Dividing X by Y - solution

---
+++ problems/beecrowd/beginner/1116.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    double x, y;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++){
+        cin >> x >> y;
+
+        if(y == 0){
+            cout << "divisao impossivel\n";
+        }
+        else{
+            cout << fixed << setprecision(1);
+            cout << x/y << endl;
+        }
+    }
+    
+    
+    return 0;
+}
