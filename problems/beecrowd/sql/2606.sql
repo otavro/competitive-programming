c40c8c5b07b286602a746576b353c9173edca670
otavro <otavio.reis366@gmail.com>
2026-05-27 01:44:55 -0300

    feat(beecrowd): add problem 2606 - Categories - solution

--- problems/beecrowd/sql/2606.sql
+++ problems/beecrowd/sql/2606.sql
@@ -1,4 +1,4 @@
-select products.id, products.name 
+select products.id, products.name
 from products
 inner join categories on categories.id = products.id_categories
 where categories.name LIKE 'super%'
