9ad3ab6c34a6f935498ba16017112c08cc14da22
otavro <otavio.reis366@gmail.com>
2026-05-27 01:45:19 -0300

    feat(beecrowd): add problem 2605 - Executive Representatives - solution

--- problems/beecrowd/sql/2605.sql
+++ problems/beecrowd/sql/2605.sql
@@ -2,4 +2,4 @@ select products.name, providers.name
 from  products
 inner join providers on providers.id = products.id_providers
 inner join categories on categories.id = products.id_categories
-where categories.id = 6
+where categories.id = 6 
