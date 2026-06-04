f28de3600bf01f8dec2173ca08e085e4374df7e6
otavro <otavio.reis366@gmail.com>
2026-06-04 17:33:42 -0300

    feat(beecrowd): add problem 1144 - Logical Sequence - solution

---
+++ problems/beecrowd/beginner/1144.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <string>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <=n; i++){
+        cout << i << " "<< i*i << " " << i*i*i << "\n";
+        cout << i << " "<< (i*i) + 1 << " " << (i*i* i) + 1 << "\n";
+    }
+
+    return 0;
+}
