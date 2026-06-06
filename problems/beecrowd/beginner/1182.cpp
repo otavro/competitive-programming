e760b2d26af2c8817698903f1f30f141f3670ca5
otavro <otavio.reis366@gmail.com>
2026-06-06 17:00:19 -0300

    feat(beecrowd): add problem 1182 - Column in Array  - solution

---
+++ problems/beecrowd/beginner/1182.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+   double m[12][12], result = 0;
+   char type;
+   int row;
+
+   cin >> row >> type;
+
+   for(int i = 0; i<12; i++){
+    for(int j = 0; j<12; j++){
+        cin >> m[i][j];
+    }
+   }
+
+    for(int i= 0; i < 12; i++){
+        result += m[i][row];
+    }
+    
+    if(type == 'M') result/=12;
+
+    cout << fixed << setprecision(1);
+    cout << result << "\n";
+
+    return 0;
+}
