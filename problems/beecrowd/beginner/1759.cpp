481fcaf4e1ae948eb8ce8bde24aa6be420e910ca
otavro <otavio.reis366@gmail.com>
2026-06-12 17:23:26 -0300

    feat(beecrowd): add problem 1759 - Ho Ho Ho  - solution

---
+++ problems/beecrowd/beginner/1759.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <string>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cin >> n;
+
+    if(n == 0)
+        return 0;
+
+    for(int i = 1; i < n; i++){
+        cout << "Ho ";
+    }
+
+    cout << "Ho!\n";
+
+    return 0;
+}
