c00b39c8e8bf7f1087681828e68982ccaa511ae0
otavro <otavio.reis366@gmail.com>
2026-06-17 11:29:48 -0300

    feat(beecrowd): add problem 1924 - Vitoria and Her indecision  - solution

---
+++ problems/beecrowd/beginner/1924.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+#include <map>
+#include <string>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    string x;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+        cin >> x;
+
+    cout << "Ciencia da Computacao\n";
+
+    return 0;
+}
